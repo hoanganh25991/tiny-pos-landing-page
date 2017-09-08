d0ccb09f1063145eb3df3108c2d0c0f2ef3e7454
Anh Le Hoang <lehoanganh25991@gmail.com>
2017-09-08 17:16:30 +0700

    First try

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Landing Page</title>
+</head>
+<body>
+<ul>
+	<li><a href="x_awesome_landing_page/index.html">Default Style</a></li>
+	<li><a href="x_awesome_landing_page/phone_app.html">Phone App</a></li>
+	<li><a href="x_awesome_landing_page/web_app.html">Web App</a></li>
+</ul>
+</body>
+</html>
